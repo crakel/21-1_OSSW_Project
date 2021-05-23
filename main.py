d52956caed1186e9be5c837381a58d1ac53cafba
김기윤 <crakel528@ajou.ac.kr>
2021-05-23 17:06:34 +0900

    Merge draw

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
-import cv2
 import dlib
+import cv2 as cv
 import numpy as np
-from scipy.spatial import distance as dist
-import imutils
-from imutils.video import VideoStream
 from imutils import face_utils
-import argparse
-import time
-
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
-capture = cv2.VideoCapture(0)
-capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cap = cv.VideoCapture(0)
 
+# range는 끝값이 포함안됨
 ALL = list(range(0, 68))
 RIGHT_EYEBROW = list(range(17, 22))
 LEFT_EYEBROW = list(range(22, 27))
@@ -27,8 +20,6 @@ MOUTH_OUTLINE = list(range(48, 61))
 MOUTH_INNER = list(range(61, 68))
 JAWLINE = list(range(0, 17))
 
-index = ALL
-
 
 # 유클리드 거리 계산 함수
 def euclidean_dist(x, y):
@@ -37,17 +28,12 @@ def euclidean_dist(x, y):
 
 # EAR 계산 함수
 def eye_aspect_ratio(eye):
-    # compute the euclidean distances between the two sets of vertical eye landmarks (x, y)-coordinates
     A = euclidean_dist(eye[1], eye[5])
     B = euclidean_dist(eye[2], eye[4])
-
-    # compute the euclidean distance between the horizontal eye landmark (x, y)-coordinates
     C = euclidean_dist(eye[0], eye[3])
 
-    # compute the eye aspect ratio
     ear = (A + B) / (2.0 * C)
 
-    # return the eye aspect ratio
     return ear
 
 
@@ -59,42 +45,48 @@ TOTAL = 0
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
-while cv2.waitKey(33) < 0:
-    ret, frame = capture.read()
-    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    dets = detector(img_gray, 1)
+index = ALL
+location_list = []
+history_list = []
+Draw = True
+
+
+def draw(img_frame, locations):
+    for i in range(len(locations) - 1):
+        if locations[0] is None or locations[1] is None:
+            continue
+
+        cv.line(img_frame, tuple(locations[i]), tuple(locations[i + 1]), (0, 255, 255), 3)
+
+    return img_frame
+
+
+while True:
+
+    ret, img_frame = cap.read()
+    h, w, c = img_frame.shape
+
+    img_draw = np.ones((h, w), dtype=np.uint8) * 255
+    img_frame = cv.flip(img_frame, 1)
+    img_gray = cv.cvtColor(img_frame, cv.COLOR_BGR2GRAY)
+
     rects = detector(img_gray, 0)
 
-    for face in dets:
+    for rect in rects:
+        shape = predictor(img_frame, rect)  # 얼굴에서 68개 점 찾기
+        eye_shape = face_utils.shape_to_np(shape)
 
-        shape = predictor(frame, face)  # 얼굴에서 68개 점 찾기
-        fshape = face_utils.shape_to_np(shape)
-
-
-        list_points = []
-        for p in shape.parts():
-            list_points.append([p.x, p.y])
-
-        list_points = np.array(list_points)
-
-        for i, pt in enumerate(list_points[index]):
-            pt_pos = (pt[0], pt[1])
-            cv2.circle(frame, pt_pos, 2, (0, 255, 0), -1)
-
-        cv2.rectangle(frame, (face.left(), face.top()), (face.right(), face.bottom()),
-                      (0, 0, 255), 3)
-        ###################################################################
-        leftEye = fshape[lStart:lEnd]
-        rightEye = fshape[rStart:rEnd]
+        leftEye = eye_shape[lStart:lEnd]
+        rightEye = eye_shape[rStart:rEnd]
         leftEAR = eye_aspect_ratio(leftEye)
         rightEAR = eye_aspect_ratio(rightEye)
 
         ear = (leftEAR + rightEAR) / 2.0
 
-        leftEyeHull = cv2.convexHull(leftEye)
-        rightEyeHull = cv2.convexHull(rightEye)
-        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+        leftEyeHull = cv.convexHull(leftEye)
+        rightEyeHull = cv.convexHull(rightEye)
+        cv.drawContours(img_frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv.drawContours(img_frame, [rightEyeHull], -1, (0, 255, 0), 1)
 
         if ear < EYE_AR_THRESH:
             COUNTER += 1
@@ -104,32 +96,75 @@ while cv2.waitKey(33) < 0:
                 TOTAL += 1
             COUNTER = 0
 
-        cv2.putText(
-            frame,
+        cv.putText(
+            img_frame,
             "Blinks: {}".format(TOTAL),
             (10, 30),
-            cv2.FONT_HERSHEY_SIMPLEX,
+            cv.FONT_HERSHEY_SIMPLEX,
             0.7,
             (0, 0, 255),
             2,
         )
 
-        cv2.putText(
-            frame,
+        cv.putText(
+            img_frame,
             "EAR: {:.2f}".format(ear),
             (300, 30),
-            cv2.FONT_HERSHEY_SIMPLEX,
+            cv.FONT_HERSHEY_SIMPLEX,
             0.7,
             (0, 0, 255),
             2,
         )
 
-    cv2.imshow("VideoFrame", frame)
+        # 그리는 부분 ############################################
+        list_points = []
+        for p in shape.parts():
+            list_points.append([p.x, p.y])
 
-    key = cv2.waitKey(1)
+        list_points = np.array(list_points)
 
-    if key == 27:
+        # for i, pt in enumerate(list_points[index]):
+        #     pt_pos = (pt[0], pt[1])
+        #     cv.circle(img_frame, pt_pos, 2, (0, 255, 0), -1)
+        #
+        # cv.rectangle(img_frame, (face.left(), face.top()), (face.right(), face.bottom()),
+        #              (0, 0, 255), 3)
+
+        nose_x = list_points[NOSE_TIP][0][0]
+        nose_y = list_points[NOSE_TIP][0][1]
+
+        if Draw:
+            location_list.append((nose_x, nose_y))
+        else:
+            history_list.append(location_list.copy())
+            location_list.clear()
+
+    img_frame = draw(img_frame, location_list)
+
+    for locations in history_list:
+        img_frame = draw(img_frame, locations)
+
+    cv.imshow('result', img_frame)
+
+    key = cv.waitKey(1)
+
+    if key == 27:  # ESC
         break
+
+    elif key == 32:  # space bar
+        location_list.clear()
+        history_list.clear()
+
+    elif key == ord('v'):  # 이니셜 포인트 지정. 눈 깜빡임 컨트롤로 바꾸기
+        Draw = not Draw
+
+    elif key == ord('e'):
+        Draw = False
+        img_draw = draw(img_draw, location_list)
+        for locations in history_list:
+            img_draw = draw(img_draw, locations)
+        # img_draw=cv.GaussianBlur(img_draw,(5,5),0)
+        cv.imshow('draw_result', img_draw)
 
     elif key == ord('0'):
         index = NOSE_TIP
@@ -146,5 +181,10 @@ while cv2.waitKey(33) < 0:
     elif key == ord('6'):
         index = JAWLINE
 
-capture.release()
-cv2.destroyAllWindows()
+cap.release()
+
+#실행 취소 버튼 만들기(눈 연속 깜빡임?)
+#입력 완료 버튼 만들기(눈 오래 감고 있기?)->입력 완료 시 그려진 그림을 내보내기
+#생각보다 움직임의 범위가 커야 함
+#고개를 숙이거나 좌우로 고개를 바꾸면 인식률이 떨어짐
+#line smoothing
