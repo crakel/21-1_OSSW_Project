dae7d1b5be9e50f8e0638e22c29a4c55ff0640a3
김기윤 <crakel528@ajou.ac.kr>
2021-05-23 16:27:00 +0900

    Add eyeblink detector

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import cv2
 import dlib
 import numpy as np
+from scipy.spatial import distance as dist
+import imutils
+from imutils.video import VideoStream
+from imutils import face_utils
+import argparse
+import time
+
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
@@ -15,21 +22,54 @@ LEFT_EYEBROW = list(range(22, 27))
 RIGHT_EYE = list(range(36, 42))
 LEFT_EYE = list(range(42, 48))
 NOSE = list(range(27, 36))
-NOSE_TIP = list(range(30,31))
+NOSE_TIP = list(range(30, 31))
 MOUTH_OUTLINE = list(range(48, 61))
 MOUTH_INNER = list(range(61, 68))
 JAWLINE = list(range(0, 17))
 
 index = ALL
 
+
+# 유클리드 거리 계산 함수
+def euclidean_dist(x, y):
+    return np.linalg.norm(x - y)
+
+
+# EAR 계산 함수
+def eye_aspect_ratio(eye):
+    # compute the euclidean distances between the two sets of vertical eye landmarks (x, y)-coordinates
+    A = euclidean_dist(eye[1], eye[5])
+    B = euclidean_dist(eye[2], eye[4])
+
+    # compute the euclidean distance between the horizontal eye landmark (x, y)-coordinates
+    C = euclidean_dist(eye[0], eye[3])
+
+    # compute the eye aspect ratio
+    ear = (A + B) / (2.0 * C)
+
+    # return the eye aspect ratio
+    return ear
+
+
+EYE_AR_THRESH = 0.3
+EYE_AR_CONSEC_FRAMES = 3
+
+COUNTER = 0
+TOTAL = 0
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
 while cv2.waitKey(33) < 0:
     ret, frame = capture.read()
     img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     dets = detector(img_gray, 1)
+    rects = detector(img_gray, 0)
 
     for face in dets:
 
         shape = predictor(frame, face)  # 얼굴에서 68개 점 찾기
+        fshape = face_utils.shape_to_np(shape)
+
 
         list_points = []
         for p in shape.parts():
@@ -42,7 +82,47 @@ while cv2.waitKey(33) < 0:
             cv2.circle(frame, pt_pos, 2, (0, 255, 0), -1)
 
         cv2.rectangle(frame, (face.left(), face.top()), (face.right(), face.bottom()),
-                     (0, 0, 255), 3)
+                      (0, 0, 255), 3)
+        ###################################################################
+        leftEye = fshape[lStart:lEnd]
+        rightEye = fshape[rStart:rEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+
+        ear = (leftEAR + rightEAR) / 2.0
+
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+        if ear < EYE_AR_THRESH:
+            COUNTER += 1
+
+        else:
+            if COUNTER >= EYE_AR_CONSEC_FRAMES:
+                TOTAL += 1
+            COUNTER = 0
+
+        cv2.putText(
+            frame,
+            "Blinks: {}".format(TOTAL),
+            (10, 30),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.7,
+            (0, 0, 255),
+            2,
+        )
+
+        cv2.putText(
+            frame,
+            "EAR: {:.2f}".format(ear),
+            (300, 30),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.7,
+            (0, 0, 255),
+            2,
+        )
 
     cv2.imshow("VideoFrame", frame)
 
@@ -68,4 +148,3 @@ while cv2.waitKey(33) < 0:
 
 capture.release()
 cv2.destroyAllWindows()
-
